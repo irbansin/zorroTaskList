1b6d3ca3aa239ad0cc514d083c5d2c7ea077b692
Anirban Sinha <asinha@inmbp015.local>
2020-10-22 11:11:43 +0530

    post scripts ready

--- src/app/data.model.ts
+++ src/app/data.model.ts
@@ -1,6 +1,20 @@
-export interface Task {
+
+export interface TaskObject {
   message: string;
   due_date?: string;
   priority?: number;
   assigned_to?: number;
+}
+export interface TaskUpdateObject extends TaskObject {
+  taskId: number;
+}
+
+export interface DeleteTaskObject {
+  taskId: number;
+}
+
+export interface Users {
+  id: number;
+  name: string;
+  picture: string;
 }
--- src/app/global.service.ts
+++ src/app/global.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
-import { Task } from './data.model';
+import { TaskObject, TaskUpdateObject, DeleteTaskObject, Users } from './data.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +12,14 @@ export class GlobalService {
   constructor(private http: HttpClient) {
   }
 
-  getTaskList(): Observable<Task[]> {
+  getTaskList(): Observable<TaskObject[]> {
     let header = new HttpHeaders().set(
       "AuthToken",
       "3UQLDSANnFDoIDMGMVjuanthCUcQopeW"
     );
     return this.http.get<any[]>('https://devza.com/tests/tasks/list', {headers:header})
   }
-  getUserList(): Observable<any[]> {
+  getUserList(): Observable<Users[]> {
     let header = new HttpHeaders().set(
       "AuthToken",
       "3UQLDSANnFDoIDMGMVjuanthCUcQopeW"
@@ -32,5 +32,19 @@ export class GlobalService {
       "3UQLDSANnFDoIDMGMVjuanthCUcQopeW"
     );
     return this.http.post<any[]>('http://devza.com/tests/tasks/create', postObject, {headers:header})
+  }
+  updateTask(postObject: TaskUpdateObject): Observable<any[]> {
+    let header = new HttpHeaders().set(
+      "AuthToken",
+      "3UQLDSANnFDoIDMGMVjuanthCUcQopeW"
+    );
+    return this.http.post<any[]>('http://devza.com/tests/tasks/update', postObject, {headers:header})
+  }
+  deleteTask(postObject: DeleteTaskObject): Observable<any[]> {
+    let header = new HttpHeaders().set(
+      "AuthToken",
+      "3UQLDSANnFDoIDMGMVjuanthCUcQopeW"
+    );
+    return this.http.post<any[]>('http://devza.com/tests/tasks/delete', postObject, {headers:header})
   }
 }
